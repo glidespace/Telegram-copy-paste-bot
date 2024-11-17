67dd47b44a6c292756228b1fd53d4e135f12b403
GLIDE SPACE <clehackweb@gmail.com>
2024-11-17 18:03:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,8 @@ from telethon.sync import TelegramClient, events
 import os
 import re
 
-from config import (API_ID,
-                    API_HASH,
-                    PHONE_NUMBER,
-                    CHANNELS_COPY,
-                    CHANNEL_PASTE,
-                    DEVICE_MODEL,
-                    SYSTEM_VERSION,
-                    AUTO_DELETE_LINKS,
-                    FORWARDS)
+from config import (API_ID, API_HASH, PHONE_NUMBER, CHANNELS_COPY, CHANNEL_PASTE, DEVICE_MODEL, SYSTEM_VERSION,
+                    AUTO_DELETE_LINKS, FORWARDS)
 
 logo = """
 █▀▀ █▀█ █▀█ █▄█ ▄▄ █▀█ ▄▀█ █▀ ▀█▀ █▀▀   █▄▄ █▀█ ▀█▀|ᵇʸ ᵈᵉˡᵃᶠᵃᵘˡᵗ
@@ -55,12 +48,12 @@ async def album_handler(event):
     """
     Обработка альбомов
     """
-    if FORWARDS is True:
+    if FORWARDS:
         if event.messages[0].fwd_from:
             pass
         else:
             return
-    elif FORWARDS is False:
+    elif not FORWARDS:
         if event.messages[0].fwd_from:
             return
 
@@ -68,16 +61,16 @@ async def album_handler(event):
     caption = event.messages[0].text
     force_document = False
     id = event.messages[0].id
+
     if id in last_id_message:
         last_id_message.clear()
-        return # Album почему-то иногда получает ивент дважды на одно сообщение.
+        return  # Avoid processing the same album twice
     last_id_message.append(id)
 
     caption = await check_caption(caption)
-
     gd_print(f"Получили альбом из {len(event)} сообщений.")
 
-    for group_message in event.messages: # Да, spoiler для альбомов в теории можно реализовать, однако сделать это не так просто, как кажется. Плюсом занимало бы много времени на обработку.
+    for group_message in event.messages:
         if group_message.photo:
             media.append(await client.download_media(group_message, f"temp_pics/{group_message.id}.png"))
         elif group_message.video:
@@ -89,12 +82,12 @@ async def album_handler(event):
         else:
             return bd_print("Неизвестный тип сообщения")
 
+    # Send the album
     await client.send_file(CHANNEL_PASTE, media, caption=caption, force_document=force_document)
     gd_print(f"Скопировали и успешно отправили альбом из {len(event)} сообщений.")
 
     for file in media:
-        os.remove(file) # использование временной папки оказалось самым удобным способом.
-
+        os.remove(file)  # Remove temporary files
 
 @client.on(events.NewMessage(CHANNELS_COPY, forwards=FORWARDS))
 async def message_handler(event):
@@ -103,64 +96,57 @@ async def message_handler(event):
     """
     if event.message.grouped_id is not None:
         return
-    
+
     id = event.message.id
     caption = event.message.text
     spoiler = False
     web_preview = False
-    
+    reply_to_msg_id = event.message.reply_to_msg_id  # Check if the message is a reply
+
     try:
-        if event.message.media.__dict__['spoiler'] is True:
+        if event.message.media.__dict__.get('spoiler', False):
             spoiler = True
     except AttributeError:
         pass
-    except KeyError:
-        pass
 
     try:
-        if event.message.media.webpage.__dict__['url'] is not None:
+        if event.message.media.webpage.__dict__.get('url') is not None:
             web_preview = True
     except AttributeError:
         pass
-    except KeyError:
-        pass
 
     gd_print(f"Получили сообщение {id}.")
-
     caption = await check_caption(caption)
 
     if event.message.photo and not web_preview:
         await client.download_media(event.message, f"temp_pics/pics_{id}.png")
-        await client.send_file(CHANNEL_PASTE, InputMediaUploadedPhoto(await client.upload_file(f"temp_pics/pics_{id}.png"), spoiler=spoiler), caption=caption)
+        await client.send_file(CHANNEL_PASTE, InputMediaUploadedPhoto(await client.upload_file(f"temp_pics/pics_{id}.png"), spoiler=spoiler), caption=caption, reply_to=reply_to_msg_id)
         os.remove(f"temp_pics/pics_{id}.png")
 
-    elif event.message.video: 
+    elif event.message.video:
         await client.download_media(event.message, f"temp_pics/pics_{id}.mp4")
-        await client.send_file(CHANNEL_PASTE, f"temp_pics/pics_{id}.mp4", caption=caption, video_note=True) # video_note позволяет отправлять кружки в виде кружков, однако из-за этого иногда GIF может ошибочно отправляться как кружок. Используйте video_note = False на своё усмотрение.
+        await client.send_file(CHANNEL_PASTE, f"temp_pics/pics_{id}.mp4", caption=caption, video_note=True, reply_to=reply_to_msg_id)
         os.remove(f"temp_pics/pics_{id}.mp4")
 
     elif event.message.document:
         file_name = next((x.file_name for x in event.message.document.attributes if isinstance(x, DocumentAttributeFilename)), None)
         if event.message.document.mime_type == "audio/ogg":
             path = await client.download_media(event.message, f"temp_pics/{id}")
-            await client.send_file(CHANNEL_PASTE, path, voice_note=True)
+            await client.send_file(CHANNEL_PASTE, path, voice_note=True, reply_to=reply_to_msg_id)
             os.remove(path)
             return
         await client.download_media(event.message, f"temp_pics/{file_name}")
-        await client.send_file(CHANNEL_PASTE, f"temp_pics/{file_name}", caption=caption, force_document=True)
+        await client.send_file(CHANNEL_PASTE, f"temp_pics/{file_name}", caption=caption, force_document=True, reply_to=reply_to_msg_id)
         os.remove(f"temp_pics/{file_name}")
 
     else:
         try:
-            await client.send_message(CHANNEL_PASTE, caption)
+            await client.send_message(CHANNEL_PASTE, caption, reply_to=reply_to_msg_id)  # Add reply_to here
         except Exception as e:
             bd_print(f"Ошибка при отправке сообщения: {e}")
             return
 
     gd_print(f"Скопировали и успешно отправили сообщение {id}.")
-
-
-
 
 if __name__ == "__main__":
     try:
