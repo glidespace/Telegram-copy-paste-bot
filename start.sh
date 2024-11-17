dc036db07ac0742a47fbb71e1546b397de2f1d4a
GLIDE SPACE <clehackweb@gmail.com>
2024-11-17 17:44:47 +0100

    Update and rename start.bat to start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clear the terminal
+clear
+
+# Print a startup message
+echo "Starting the script..."
+
+# Wait for a few seconds
+sleep 3
+
+# Run the Python script
+python3 main.py
